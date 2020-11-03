3dbdda5268cd1c64bc73224512e65ed362bc8134
Johannes Konings <johannes.konings@outlook.com>
2020-11-03 15:05:03 +0100

    sum calculation added

--- node/src/calculateData.js
+++ node/src/calculateData.js
@@ -5,11 +5,28 @@ const fs = require('fs');
 
     const data = await convertData2Json();
 
-    console.log("data", data);
+    const total = data.reduce((acc, curr) => {
+        if (!acc[curr.Meldedatum]) {
+            acc[curr.Meldedatum] = { sum: parseInt(curr.AnzahlFall) };
+            return acc;
+        }
+        acc[curr.Meldedatum].sum += parseInt(curr.AnzahlFall);
+
+        return acc;
+    }, {});
+
+    const sortObject = Object.entries(total).sort().reduce( (o,[k,v]) => (o[k]=v,o), {} )
+
+    //console.log("total", sortObject);
 
     var file = fs.createWriteStream('./../_data/RKI_COVID19_aggregated.csv');
     file.on('error', function (err) { Console.log(err) });
-    file.write('test');
+    file.write('Datum, Anzahl Fälle\n' );
+    Object.entries(sortObject).forEach(([key, val]) => {
+        console.log(key); // the name of the current key.
+        console.log(val.sum); // the value of the current key.
+        file.write(key + ',' + val.sum + '\n');
+      });
     file.end();
 
 })();
