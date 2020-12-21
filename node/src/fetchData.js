95e25157fbdb0af5db83be07f66dc2d0334736ae
Johannes Konings <johannes.konings@outlook.com>
2020-12-21 18:36:22 +0100

    https get with redirect added

--- node/src/fetchData.js
+++ node/src/fetchData.js
@@ -1,8 +1,8 @@
-const http = require('https');
+const https = require('follow-redirects').https;
 const fs = require('fs');
 
-const file = fs.createWriteStream("./../raw-data/RKI_COVID19.csv");
-const request = http.get("https://npgeo-de.maps.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data", function (response) {
-    console.log("start to fetch data");
-    response.pipe(file);
-});
+const url = 'https://npgeo-de.maps.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data';
+const filePath = './../raw-data/RKI_COVID19.csv';
+
+https.get(url, resp => resp.pipe(fs.createWriteStream(filePath)));
+
