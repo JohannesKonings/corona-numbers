dbf0d14c3118a1c92e012d876213d3aff1c49e6f
Johannes Konings <johannes.konings@outlook.com>
2021-01-11 18:35:57 +0100

    fetch also Vaccinations

--- node/src/fetchData.js
+++ node/src/fetchData.js
@@ -1,8 +1,23 @@
 const https = require('follow-redirects').https;
 const fs = require('fs');
 
-const url = 'https://npgeo-de.maps.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data';
-const filePath = './../raw-data/RKI_COVID19.csv';
+const fetchUrl = (url, filePath) => {
+    console.log('fetch:', url);
+    https.get(url, resp => resp.pipe(fs.createWriteStream(filePath)));
+}
 
-https.get(url, resp => resp.pipe(fs.createWriteStream(filePath)));
+const fetch = async () => {
+    let url;
+    let filePath;
+
+    url = 'https://npgeo-de.maps.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data';
+    filePath = './../raw-data/RKI_COVID19.csv';
+    fetchUrl(url, filePath);
+
+    url = 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx;jsessionid=E77F3BDDD7B9730B5EACCD0110AA76D5.internet092?__blob=publicationFile';
+    filePath = './../raw-data/Impfquotenmonitoring.xlsx';
+    fetchUrl(url, filePath);
+};
+
+fetch();
 
